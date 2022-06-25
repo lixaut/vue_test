cb50ac304d79a9c20e4a0acfbe92bd7f0d89a1df
Berg Qin <wlkp9108@gmail.com>
2022-06-25 19:30:42 +0800

    vue脚手架

---
+++ src/components/School.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return {
+        name: 'XAUT',
+        address: 'xian'
+      }
+    },
+    methods: {
+      showName() {
+        alert(this.name)
+      }
+    }
+  }
+</script>
+
+<style>
+  .demo {
+    background-color: orange;
+  }
+</style>
