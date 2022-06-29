2c2efc5004e6fcc2460704fe31a897f25613e699
Berg Qin <wlkp9108@gmail.com>
2022-06-29 17:30:21 +0800

    自定义事件绑定

---
+++ 07_src_组件自定义事件/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="sendSchoolName">把学校名给App</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return {
+        name: 'XAUT',
+        address: 'xian',
+      }
+    },
+    props: ['getSchoolName'],
+    methods: {
+      sendSchoolName() {
+        this.getSchoolName(this.name)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .demo {
+    background-color: orange;
+  }
+</style>
